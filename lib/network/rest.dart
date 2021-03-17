30bf01c5f6bc6e671eadfc7720efedd9e881005a
Mario <mario@mobills.com.br>
2021-03-17 10:35:07 -0300

    fazendo request de exemplo

---
+++ lib/network/rest.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+abstract class RestManager {
+  static const String _url = 'https://blog.cheesecakelabs.com/challenge/';
+
+  static Future<List<Map<String, dynamic>>> get() async {
+    final http.Response r = await http.get(Uri.parse(_url));
+
+    if (r.statusCode == 200) {
+      return (jsonDecode(r.body) as List<dynamic>)
+          .map((e) => e as Map<String, dynamic>)
+          .toList();
+    } else {
+      throw Exception(
+          'error requesting $_url: [${r.statusCode}] ${r.reasonPhrase}');
+    }
+  }
+}
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:teste_cheesecake/model/article.dart';
+import 'package:teste_cheesecake/network/rest.dart';
 
 class HomeView extends StatelessWidget {
   HomeView({Key? key}) : super(key: key);
@@ -7,6 +9,18 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+
+    RestManager.get().then((value) {
+      final List<Article> list = value.map((e) => Article.fromJson(e)).toList();
+
+      list.forEach((e) {
+        print(e.content);
+      });
+    }).catchError((e, s) {
+      print('deu ruim');
+      print(e);
+      print(s);
+    });
 
     return Scaffold(
       appBar: AppBar(
