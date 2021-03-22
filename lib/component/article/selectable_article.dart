bb860b1a5995f4ada20bf78d7fc0a06a10c1172d
lord <m_s_r@outlook.com>
2021-03-22 09:29:33 -0300

    add lib de imagens

--- lib/component/article/selectable_article.dart
+++ lib/component/article/selectable_article.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:teste_cheesecake/model/article.dart';
@@ -10,12 +11,27 @@ class SelectableArticleComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size _size = MediaQuery.of(context).size;
+    final double _boxSize = _size.width;
+
     return InkWell(
       child: Container(
         padding: EdgeInsets.all(10),
         child: Row(
           children: [
-            Text(article.title),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5))),
+              width: 50.0,
+              height: 50.0,
+              child: CachedNetworkImage(
+                imageUrl: article.image_url,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Text(
+              article.title,
+            ),
           ],
         ),
       ),
