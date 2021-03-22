8aee1d0e61e6845c17c1102925ff374339772ca2
Mario <mario@mobills.com.br>
2021-03-22 16:34:25 -0300

    marcando artigo como lido

--- lib/component/article/selectable_article.dart
+++ lib/component/article/selectable_article.dart
@@ -1,30 +1,30 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 import 'package:teste_cheesecake/component/text/overflow_text.dart';
 import 'package:teste_cheesecake/component/text/side_by_side_text.dart';
 import 'package:teste_cheesecake/consts.dart';
-import 'package:teste_cheesecake/model/article.dart';
+import 'package:teste_cheesecake/model/custom_article.dart';
 import 'package:teste_cheesecake/routes.dart';
+import 'package:teste_cheesecake/state/app_state.dart';
 
 class SelectableArticleComponent extends StatelessWidget {
-  final Article article;
-  final bool read;
+  final int index;
+  final CustomArticle article;
 
-  SelectableArticleComponent(
-      {Key? key, required this.article, this.read = true})
-      : super(key: key);
+  SelectableArticleComponent({required this.index, required this.article});
 
   @override
   Widget build(BuildContext context) {
     const double _max_size = 100.0;
+    final AppState appState = Provider.of(context);
     final Size _size = MediaQuery.of(context).size;
     final double _proportion = _size.width * 0.2;
     final double _boxSize = _proportion > _max_size ? _max_size : _proportion;
     final double _padding = _boxSize * 0.2;
     final double _radius = _boxSize * 0.1;
     final double _height = _boxSize + _padding;
-    final String _hero = 'hero-${article.image_url}';
+    final String _hero = 'hero-${article.article.image_url}';
 
     return InkWell(
       child: Container(
@@ -42,7 +42,7 @@ class SelectableArticleComponent extends StatelessWidget {
                 child: Hero(
                   tag: _hero,
                   child: CachedNetworkImage(
-                    imageUrl: article.image_url,
+                    imageUrl: article.article.image_url,
                     fit: BoxFit.cover,
                   ),
                 ),
@@ -55,14 +55,14 @@ class SelectableArticleComponent extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     OverflowableTextComponent(
-                      text: article.title,
+                      text: article.article.title,
                       maxLines: 2,
-                      bold: read,
+                      bold: !article.read,
                     ),
                     Spacer(),
                     SideBySideTextComponent(
-                      first: article.authors,
-                      second: article.date,
+                      first: article.article.authors,
+                      second: article.article.date,
                     ),
                   ],
                 ),
@@ -72,8 +72,10 @@ class SelectableArticleComponent extends StatelessWidget {
         ),
       ),
       onTap: () {
+        appState.setRead(index);
+
         Navigator.of(context).pushNamed(AppRoutes.article, arguments: {
-          AppConsts.articleArg: article,
+          AppConsts.articleArg: article.article,
           AppConsts.articleHero: _hero
         });
       },
