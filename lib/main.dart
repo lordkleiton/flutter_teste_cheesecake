e49aa6c50dfbcd616145e60239362055dc400350
Mario <mario@mobills.com.br>
2021-03-22 16:11:36 -0300

    fazendo requisição inicial e configurando provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:teste_cheesecake/router.dart';
+import 'package:teste_cheesecake/state/app_state.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => AppState()),
+    ],
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    final AppState appState = Provider.of(context);
+
+    appState.setup();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Teste Cheesecake',
       onGenerateRoute: AppRouter.routeGenerator,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
     );
