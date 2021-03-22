35c402bdea734367ffbc36ed737c3224048978b0
Mario <mario@mobills.com.br>
2021-03-22 18:15:01 -0300

    mesma coisa do commit anterior

--- lib/component/article/selectable_article.dart
+++ lib/component/article/selectable_article.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
 import 'package:teste_cheesecake/component/text/overflow_text.dart';
 import 'package:teste_cheesecake/component/text/side_by_side_text.dart';
@@ -16,69 +17,92 @@ class SelectableArticleComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const double _max_size = 100.0;
+    const double max_size = 100.0;
     final AppState appState = Provider.of(context);
-    final Size _size = MediaQuery.of(context).size;
-    final double _proportion = _size.width * 0.2;
-    final double _boxSize = _proportion > _max_size ? _max_size : _proportion;
-    final double _padding = _boxSize * 0.2;
-    final double _radius = _boxSize * 0.1;
-    final double _height = _boxSize + _padding;
-    final String _hero = 'hero-${article.article.image_url}';
+    final Size size = MediaQuery.of(context).size;
+    final double proportion = size.width * 0.2;
+    final double boxSize = proportion > max_size ? max_size : proportion;
+    final double padding = boxSize * 0.2;
+    final double radius = boxSize * 0.1;
+    final double height = boxSize + padding;
+    final String hero = 'hero-${article.article.image_url}';
+    final bool read = article.read;
+    final Widget slideAction = SlideAction(
+      color: read ? Colors.blue : Colors.green,
+      onTap: () {
+        appState.toggleRead(index);
+      },
+      child: Text(
+        read ? 'Mark as unread' : 'Mark as read',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
 
-    return InkWell(
-      child: Container(
-        height: _height,
-        padding: EdgeInsets.all(_padding),
-        child: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.all(
-                Radius.circular(_radius),
-              ),
-              child: Container(
-                width: _boxSize,
-                height: _boxSize,
-                child: Hero(
-                  tag: _hero,
-                  child: CachedNetworkImage(
-                    imageUrl: article.article.image_url,
-                    fit: BoxFit.cover,
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actions: [
+        slideAction,
+      ],
+      secondaryActions: [
+        slideAction,
+      ],
+      child: InkWell(
+        child: Container(
+          height: height,
+          padding: EdgeInsets.all(padding),
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(radius),
+                ),
+                child: Container(
+                  width: boxSize,
+                  height: boxSize,
+                  child: Hero(
+                    tag: hero,
+                    child: CachedNetworkImage(
+                      imageUrl: article.article.image_url,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.only(left: _padding),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    OverflowableTextComponent(
-                      text: article.article.title,
-                      maxLines: 2,
-                      bold: !article.read,
-                    ),
-                    Spacer(),
-                    SideBySideTextComponent(
-                      first: article.article.authors,
-                      second: article.article.date,
-                    ),
-                  ],
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(left: padding),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      OverflowableTextComponent(
+                        text: article.article.title,
+                        maxLines: 2,
+                        bold: !read,
+                      ),
+                      Spacer(),
+                      SideBySideTextComponent(
+                        first: article.article.authors,
+                        second: article.article.date,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
-      ),
-      onTap: () {
-        appState.setRead(index);
+        onTap: () {
+          appState.setRead(index);
 
-        Navigator.of(context).pushNamed(AppRoutes.article, arguments: {
-          AppConsts.articleArg: article.article,
-          AppConsts.articleHero: _hero
-        });
-      },
+          Navigator.of(context).pushNamed(AppRoutes.article, arguments: {
+            AppConsts.articleArg: article.article,
+            AppConsts.articleHero: hero
+          });
+        },
+      ),
     );
   }
 }
