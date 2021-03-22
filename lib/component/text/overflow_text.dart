32880754212f07c1b2322f84a2ef8f40e2321249
Mario <mario@mobills.com.br>
2021-03-22 13:27:12 -0300

    passando estilo custom de texto se necessario

--- lib/component/text/overflow_text.dart
+++ lib/component/text/overflow_text.dart
@@ -4,26 +4,30 @@ class OverflowableTextComponent extends StatelessWidget {
   final String text;
   final bool bold;
   final int maxLines;
-  final bool subtitle;
+  final TextStyle? customStyle;
 
   OverflowableTextComponent(
       {required this.text,
       this.bold = false,
       this.maxLines = 1,
-      this.subtitle = false});
+      this.customStyle});
 
   @override
   Widget build(BuildContext context) {
+    final bool hasCustom = customStyle != null;
+    final bool isBold = !hasCustom && bold;
+    final TextStyle? noCustom = isBold
+        ? TextStyle(
+            fontWeight: FontWeight.bold,
+          )
+        : null;
+
     return Text(
       text,
       maxLines: maxLines,
       softWrap: true,
       overflow: TextOverflow.ellipsis,
-      style: bold
-          ? TextStyle(
-              fontWeight: FontWeight.bold,
-            )
-          : null,
+      style: hasCustom ? customStyle : noCustom,
     );
   }
 }
