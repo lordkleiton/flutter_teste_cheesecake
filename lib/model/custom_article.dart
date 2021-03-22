f0f229fc34bfb72460a287b63d7552df46db3223
Mario <mario@mobills.com.br>
2021-03-22 10:57:30 -0300

    artigo que pode ser lido

---
+++ lib/model/custom_article.dart
@@ -0,0 +1,8 @@
+import 'package:teste_cheesecake/model/article.dart';
+
+class CustomArticle {
+  final Article article;
+  final bool read;
+
+  CustomArticle({required this.article, this.read = false});
+}
